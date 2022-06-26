1a24886c8fa730f331d346484671d48223f7bb02
Vika <vpredko@mail.ru>
2022-06-26 14:48:25 +0300

    Added task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32 (Console.ReadLine());
+int max = number_1;
+
+if (number_1 > number_2)
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> max = " + number_1);
+else
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> max = " + number_2);
