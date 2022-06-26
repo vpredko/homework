300aee33afd1b30ae61121626833d68c357a99f4
Vika <vpredko@mail.ru>
2022-06-26 15:02:25 +0300

    Added task3

---
+++ task1/task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task 3
+Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32 (Console.ReadLine());
+
+if (number_1 % 2 == 0)
+    Console.WriteLine(number_1 + " -> " + "yes");
+else
+    Console.WriteLine(number_1 + " -> " + "no");
